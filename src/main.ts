a7c45297af00c0ec129aa722ea147dfb95cfa4c1
Patricio Ferraggi <pattferraggi@gmail.com>
2018-10-26 19:31:12 +0200

    added tslint and prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import 'phaser';
+import "phaser";
 
-import TestScene from './scenes/PlayScene';
+import TestScene from "./scenes/play.scene";
 
-const config:GameConfig = {
-    type: Phaser.AUTO,
-    parent: 'content',
-    width: 640,
-    height: 480,
-    resolution: 1, 
-    backgroundColor: "#EDEEC9",
-    scene: [
-      TestScene
-    ]
+const config: GameConfig = {
+  type: Phaser.AUTO,
+  parent: "content",
+  width: 640,
+  height: 480,
+  resolution: 1,
+  backgroundColor: "#EDEEC9",
+  scene: [TestScene]
 };
 
 new Phaser.Game(config);
---
+++ src/scenes/play.scene.ts
@@ -0,0 +1,46 @@
+class TestScene extends Phaser.Scene {
+  private player: Phaser.GameObjects.Sprite;
+  private cursors: any;
+
+  constructor() {
+    super({
+      key: "TestScene"
+    });
+  }
+
+  public preload() {
+    this.load.tilemapTiledJSON("map", "/assets/tilemaps/desert.json");
+    this.load.image("Desert", "/assets/tilemaps/tmw_desert_spacing.png");
+    this.load.image("player", "/assets/sprites/mushroom.png");
+  }
+
+  public create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("Desert");
+    const layer = map.createStaticLayer(0, tileset, 0, 0);
+
+    this.player = this.add.sprite(100, 100, "player");
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player, false);
+  }
+
+  public update(time: number, delta: number) {
+    this.player.angle += 1;
+    if (this.cursors.left.isDown) {
+      this.player.x -= 5;
+    }
+    if (this.cursors.right.isDown) {
+      this.player.x += 5;
+    }
+    if (this.cursors.down.isDown) {
+      this.player.y += 5;
+    }
+    if (this.cursors.up.isDown) {
+      this.player.y -= 5;
+    }
+  }
+}
+
+export default TestScene;
